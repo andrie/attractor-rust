41e08440b4bade073dae46718692b19ec0965f1e
andrie <apdevries@gmail.com>
2023-01-04 16:45:24 +0200

    Some refactoring and cleanup

--- src/attractor.rs
+++ src/attractor.rs
@@ -1,3 +1,4 @@
+// use image::Rgba;
 use ndarray::prelude::*;
 // use ndarray::Array;
 // use ndarray_rand::RandomExt;
@@ -8,7 +9,8 @@ use inc_stats::Percentiles as Percentiles;
 use image::{RgbaImage, ImageBuffer};
 
 pub struct Xy {
-    pub image: Array2<f32>,
+    // pub image: Array2<f32>,
+    pub filename: String,
     pub n: usize
 }
 
@@ -44,10 +46,46 @@ fn sprott(a:&Array1<f64>, &x:&f64, &y:&f64) -> (f64, f64) {
     return (x1, y1)
 }
 
+
+fn compute_percentiles(xy: &Vec<(f64, f64)>, perc_threshold: &f64) -> (Vec<f64>, Vec<f64>) {
+    // compute percentiles for xrange and yrange
+
+    let mut xperc = Percentiles::new();
+    let mut yperc = Percentiles::new();
+    for elem in xy {
+        let (x, y) = elem;
+        xperc.add(x);
+        yperc.add(y);
+    }
+    let percs = [(1.0 - perc_threshold) / 2.0, perc_threshold + (1.0 - perc_threshold) / 2.0];
+    let xrange = xperc.percentiles(&percs).unwrap().unwrap();
+    let yrange = yperc.percentiles(&percs).unwrap().unwrap();
+    return (xrange, yrange)
+}
+
+fn discretize_image(xy: &Vec<(f64, f64)>, img_size: (usize, usize), 
+    xrange: Vec<f64>, yrange: Vec<f64>, mut image:Array2<i32>) -> Array2<i32>{
+    let (mut xsize, mut ysize) = img_size; 
+    xsize -= 1;
+    ysize -= 1;
+    // println!("x and y size: {}, {}", &xsize, &ysize);
+    for elem in xy {
+        let (x, y) = elem; 
+        let xx:isize = remap(&x, &xrange[0], &xrange[1], &0.0, &(xsize as f64)) as isize;
+        let yy:isize = remap(&y, &yrange[0], &yrange[1], &0.0, &(ysize as f64)) as isize;
+        if xx >= 0 && yy >= 0 && xx <= xsize as isize && yy <= ysize as isize {
+            image[[xx as usize, yy as usize]] += 1;
+        }
+    }
+    return image;
+}
+
+
+/// 
 pub fn attractor(a:&Array1<f64>, n:&usize, x0:f64, y0:f64, 
     img_size: (usize, usize), perc_threshold: f64) -> Xy { 
 
-    let mut xy:Vec<(f64, f64)> = Vec::new();
+    let mut xy:Vec<(f64, f64)> = Vec::with_capacity(*n);
 
     xy.push((x0, y0));
 
@@ -80,65 +118,52 @@ pub fn attractor(a:&Array1<f64>, n:&usize, x0:f64, y0:f64,
         i += 1;
     }
 
-    // compute percentiles for xrange and yrange
+    // compute percentiles
 
-    let mut xperc = Percentiles::new();
-    let mut yperc = Percentiles::new();
-    for elem in &xy {
-        let (x, y) = elem;
-        xperc.add(x);
-        yperc.add(y);
-    }
-    let percs = [(1.0 - perc_threshold) / 2.0, perc_threshold + (1.0 - perc_threshold) / 2.0];
-    let xrange = xperc.percentiles(&percs).unwrap().unwrap();
-    let yrange = yperc.percentiles(&percs).unwrap().unwrap();
+    let xrange: Vec<f64>;
+    let yrange: Vec<f64>;
 
+    (xrange, yrange) = compute_percentiles(&xy, &perc_threshold);
+
+    
     // discretize into image
     let mut image: Array2<i32> = Array2::zeros(img_size);
     if finite {
         nn = *n;
-        let (mut xsize, mut ysize) = img_size; 
-        xsize -= 1;
-        ysize -= 1;
-        // println!("x and y size: {}, {}", &xsize, &ysize);
-        for elem in xy {
-            let (x, y) = elem; 
-            let xx:isize = remap(&x, &xrange[0], &xrange[1], &0.0, &(xsize as f64)) as isize;
-            let yy:isize = remap(&y, &yrange[0], &yrange[1], &0.0, &(ysize as f64)) as isize;
-            if xx >= 0 && yy >= 0 && xx <= xsize as isize && yy <= ysize as isize {
-                image[[xx as usize, yy as usize]] += 1;
-            }
-        }
+        image = discretize_image(&xy, img_size, xrange, yrange, image);
     }
 
-    let image = save_attractor(image);
+    let imgbuf:RgbaImage = convert_to_image(image);
+
+    let filename: String = save_attractor(imgbuf);
     
-    return Xy {image:image, n:nn};
+    return Xy {filename, n:nn};
  }
 
-pub fn save_attractor(image: Array2<i32>) -> Array2<f32> {
+
+fn convert_to_image(image: Array2<i32>) -> RgbaImage {
     let shape = image.shape();
     let imgx: u32 = shape[0] as u32;
     let imgy: u32 = shape[1] as u32;
-
+    
     let img_max = *image.iter().max().unwrap() as f32;
     let raw_img:Array2<f32> = image.mapv(|x| ((x as f32 / img_max) * 1.0_f32.exp()).ln_1p());
     raw_img.as_standard_layout();
-
+    
     let grad = colorgrad::rainbow();
     let mut imgbuf: RgbaImage = ImageBuffer::new(imgx, imgy);
-
-    let mut maxt = 0.0;
+    
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
         let t = raw_img[[x as usize, y as usize]] / 1.0_f32.exp().ln_1p();
-        if t > maxt { maxt = t; }
-        let rgba = grad.at(t as f64).to_rgba8();
+        let mut rgba = grad.at(t as f64).to_rgba8();
+        if t == 0.0 {rgba = [0,0,0,255]};
         *pixel = image::Rgba(rgba);
     }
+    return imgbuf;
+}
 
-    println!("maxt = {}", maxt);
-
-    imgbuf.save("attractor.png").unwrap();
-
-    return raw_img;
+fn save_attractor(imgbuf: RgbaImage) -> String {
+    let filename = String::from("attractor.png");
+    imgbuf.save(&filename).unwrap();
+    return filename;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,22 @@ use ndarray::array;
 // use ndarray::prelude::*;
 // use ndarray_rand::RandomExt;
 // use ndarray_rand::rand_distr::Uniform;
-extern crate raster;
+// extern crate raster;
 
 pub mod attractor;
 use crate::attractor::Xy;
 
 
 fn main() {
-    let n:usize = 5_000_000;
+    println!("Running code");
+    let n:usize = 100_000_000;
     // let a = Array::random(16, Uniform::new(0., 1.));
     let a = array!(-0.589143526796629, 0.674281478192589, 0.738887963849125, -0.303279513858981, 0.777634792527011, 0.0712083741377903, 0.0940309871410827, -1.17350599974869, -0.780243673606325, 0.536210512392793, 1.60723417066195, -1.2742777716953, -0.0660871384554965, 0.448090598863179);
     let xy:Xy = attractor::attractor(&a, &n, 1.0, 1.0, 
-        (1024 * 2, 768 * 2), 0.7);
+        (1024 * 4, 768 * 4), 0.7);
 
     let nn: usize = xy.n;
 
     println!("nn: {}", nn);
+    println!("saved to {}", xy.filename);
 }
