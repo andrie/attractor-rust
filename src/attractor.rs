a27fcdad2b57cc4cc77d4b5335516ef19afad331
andrie <apdevries@gmail.com>
2023-01-09 14:25:11 +0200

    Use iterator in imgbug conversion loop

--- src/attractor.rs
+++ src/attractor.rs
@@ -20,7 +20,7 @@ use ndarray::prelude::*;
 use ndarray::Array1;
 use inc_stats::Percentiles as Percentiles;
 use image::{RgbaImage, ImageBuffer};
-
+use indicatif::ProgressBar;
 
 /// Holds attractor results.
 /// 
@@ -58,12 +58,11 @@ fn remap(x:&f64, xmin:&f64, xmax:&f64, ymin:&f64, ymax:&f64) -> f64 {
     return ymin + (x - xmin) * (ymax - ymin) / (xmax - xmin);
 }
 
-
 /// Generate strange attractor using equation 7E of Sprott.
 /// 
 /// This attractor is based on Equation 7E of Sprott (see references).
 ///
-/// \deqn{x_{i+1} = a_{1} + a_{2} * x_{i} +  a_{3} * y_{i} +  a_{4} * |x_{i}|^{a5}  +  a6 * |y_{i}|^{a7}}
+/// $ x_{i+1} = a_{1} + a_{2} * x_{i} +  a_{3} * y_{i} +  a_{4} * |x_{i}|^{a5}  +  a6 * |y_{i}|^{a7} $
 ///
 /// \deqn{y_{i+1} = a_{8} + a_{9} * x_{i} +  a_{10} * y_{i} +  a_{11} * |x_{i}|^{a12}  +  a13 * |y_{i}|^{a14}}
 ///
@@ -130,7 +129,9 @@ pub fn attractor(config: &AttractorInput) -> AttractorResult {
     }
 
     // compute nn new samples
-    for i in 1..n {
+    let progress_bar = ProgressBar::new((n / 100_000) as u64);
+    for i in 1..1_000_000 {
+        if i % 100_000 == 0 { progress_bar.inc(1); }
         (x1, y1) = sprott_7e(&a, &x1, &y1);
         if x1.is_infinite() || y1.is_infinite() {
             finite = false;
@@ -151,45 +152,96 @@ pub fn attractor(config: &AttractorInput) -> AttractorResult {
 
     // discretize into image
     let mut image: Array2<i32> = Array2::zeros(img_size);
-    image = discretize_image(&xy, img_size, xrange, yrange, image);
+    discretize_image(&mut image, &xy, img_size, &xrange, &yrange);
+
+    let (mut xsize, mut ysize) = img_size; 
+    xsize -= 1;
+    ysize -= 1;
+
+    for _i in 1_000_000..n {
+        if _i % 100_000 == 0 { progress_bar.inc(1); }
+        (x1, y1) = sprott_7e(&a, &x1, &y1);
+        remap_element(&mut image, &(x1, y1), &xrange[0], &xrange[1], 
+            &yrange[0], &yrange[1], &xsize, &ysize)
+    }
+    progress_bar.finish();
 
     // convert array to image with colour gradient
+    println!("Converting array to image...");
     let imgbuf:RgbaImage = convert_to_image(image);
 
     // save png image
+    println!("Saving image...");
     save_attractor(imgbuf, &config.filename);
     
     let filename = config.filename.to_string();
     return AttractorResult {filename: filename, n:nn, finite: finite};
- }
+}
 
 
+
+/// Remaps an xy element into a matrix location and increments the pixel count
+fn remap_element(image: &mut Array2<i32>, elem: &(f64, f64), 
+    xmin: &f64, xmax: &f64, ymin: &f64, ymax: &f64,
+    xsize: &usize, ysize: &usize) {
+    let (x, y) = elem; 
+    let xx:isize = remap(&x, &xmin, &xmax, &0.0, &(*xsize as f64)) as isize;
+    let yy:isize = remap(&y, &ymin, &ymax, &0.0, &(*ysize as f64)) as isize;
+    if xx >= 0 && yy >= 0 && xx <= *xsize as isize && yy <= *ysize as isize {
+        image[[xx as usize, yy as usize]] += 1;
+    }
+}
+
+/// Discretizes an xy array into an image
+fn discretize_image(image: &mut Array2<i32>, xy: &Vec<(f64, f64)>, img_size: (usize, usize), 
+    xrange: &Vec<f64>, yrange: &Vec<f64>){
+    let (mut xsize, mut ysize) = img_size; 
+    xsize -= 1;
+    ysize -= 1;
+    for elem in xy {
+        remap_element(image, &elem, &xrange[0], &xrange[1], 
+            &yrange[0], &yrange[1], &xsize, &ysize);
+    }
+}
+
+/// Convert array to RgbaImage
 fn convert_to_image(image: Array2<i32>) -> RgbaImage {
     let shape = image.shape();
     let imgx: u32 = shape[0] as u32;
     let imgy: u32 = shape[1] as u32;
     
+    println!("Computing max value...");
     let img_max = *image.iter().max().unwrap() as f32;
+    println!("Rescaling values...");
     let raw_img:Array2<f32> = image.mapv(|x| ((x as f32 / img_max) * 1.0_f32.exp()).ln_1p());
-    raw_img.as_standard_layout();
+    println!("Converting to standard layout...");
+    let raw_img = raw_img.reversed_axes();
     
     let grad = colorgrad::rainbow();
     let mut imgbuf: RgbaImage = ImageBuffer::new(imgx, imgy);
     
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let t = raw_img[[x as usize, y as usize]] / 1.0_f32.exp().ln_1p();
-        let mut rgba = grad.at(t as f64).to_rgba8();
-        if t == 0.0 {rgba = [0,0,0,255]};
-        *pixel = image::Rgba(rgba);
-    }
+    println!("Converting to rgba image...");
+    
+    for (p, v) in imgbuf.pixels_mut()
+        .zip(raw_img.iter()) {
+            let t = (*v as f64) / 1.0_f64.exp().ln_1p();
+            let mut rgba = [0,0,0,255];
+            if t != 0.0 {
+                rgba = grad.at(t).to_rgba8();
+            }
+            *p = image::Rgba(rgba);
+        }
+        
     return imgbuf;
 }
 
+/// Save RgbaImage to png file
 fn save_attractor(imgbuf: RgbaImage, filename: &String) {
     // let filename = String::from("attractor.png");
     imgbuf.save(filename).unwrap();
 }
 
+/// Given a vector and a threshold, compute percentile cutofss
 fn compute_percentiles(xy: &Vec<(f64, f64)>, perc_threshold: &f64) -> (Vec<f64>, Vec<f64>) {
     // compute percentiles for xrange and yrange
 
@@ -204,22 +256,5 @@ fn compute_percentiles(xy: &Vec<(f64, f64)>, perc_threshold: &f64) -> (Vec<f64>,
     let xrange = xperc.percentiles(&percs).unwrap().unwrap();
     let yrange = yperc.percentiles(&percs).unwrap().unwrap();
     return (xrange, yrange)
-}
-
-fn discretize_image(xy: &Vec<(f64, f64)>, img_size: (usize, usize), 
-    xrange: Vec<f64>, yrange: Vec<f64>, mut image:Array2<i32>) -> Array2<i32>{
-    let (mut xsize, mut ysize) = img_size; 
-    xsize -= 1;
-    ysize -= 1;
-    // println!("x and y size: {}, {}", &xsize, &ysize);
-    for elem in xy {
-        let (x, y) = elem; 
-        let xx:isize = remap(&x, &xrange[0], &xrange[1], &0.0, &(xsize as f64)) as isize;
-        let yy:isize = remap(&y, &yrange[0], &yrange[1], &0.0, &(ysize as f64)) as isize;
-        if xx >= 0 && yy >= 0 && xx <= xsize as isize && yy <= ysize as isize {
-            image[[xx as usize, yy as usize]] += 1;
-        }
-    }
-    return image;
 }
 
