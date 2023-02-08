8c23c4d144674516962447185245ed8516173951
andrie <apdevries@gmail.com>
2023-02-08 07:49:32 +0000

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 // use ndarray_rand::rand_distr::Uniform;
 // extern crate raster;
 
-use attractor::{AttractorResult, Attractor};
+use attractor::{Attractor, AttractorResult};
 
 use colorgrad::Color;
 
@@ -17,13 +17,13 @@ fn g() -> colorgrad::Gradient {
         ])
         .build()
         .unwrap();
-    }
+}
 
 fn main() {
     let now = std::time::Instant::now();
     // let a = Array::random(16, Uniform::new(0., 1.));
     // let a = array!(-0.589143526796629, 0.674281478192589, 0.738887963849125, -0.303279513858981, 0.777634792527011, 0.0712083741377903, 0.0940309871410827, -1.17350599974869, -0.780243673606325, 0.536210512392793, 1.60723417066195, -1.2742777716953, -0.0660871384554965, 0.448090598863179);
-    
+
     // set scaling parameter - higher means larger image with more iterations
     // let scale:i32 = 1;
 
@@ -33,18 +33,18 @@ fn main() {
     //     n: 4_000_000 * scale.pow(2) as usize,
     //     filename: String::from("attractor.png"),
     //     xy: (1.0, 1.0),
-    //     img_size: (1920 * scale as usize, 1080 * scale as usize), 
+    //     img_size: (1920 * scale as usize, 1080 * scale as usize),
     //     perc_threshold: 0.83,
     //     gradient_fn: colorgrad::turbo
     //     // gradient_fn: colorgrad::reds
     //     // gradient_fn: g
     // };
-    
+
     let mut sprott = Attractor::default();
     sprott.n = 1_000_000;
     println!("Computing attractor with {} iterations", sprott.n);
 
-    let xy:AttractorResult = sprott.compute();
+    let xy: AttractorResult = sprott.compute();
 
     println!("Completed in {:.2} seconds", now.elapsed().as_secs_f32());
 
@@ -55,5 +55,4 @@ fn main() {
         println!("Encountered infinity");
         println!("nn: {}", nn);
     }
-
 }
